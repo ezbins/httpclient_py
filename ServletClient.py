5b1974e1dbc7c911b6d747782ef8bad6ca18cfba
Dennis Peng <ty3627@gmail.com>
2016-10-03 16:20:07 +0800

    增加程式的註解

--- ServletClient.py
+++ ServletClient.py
@@ -11,12 +11,16 @@ data ={'intValue':value}
 json_data = json.dumps(data).encode('utf-8')
 print(json_data)
 
-#傳送json data以binary 方式傳送
+"""
+Python傳送json data以binary 方式傳送,所以Java Servlet端接收
+需要InputStream方式接收
+"""
 res = http.request(
     'POST',
     'http://localhost:8080/PythonServlet/Main',
     body=json_data,
     headers={'Content-Type':'application/json'})
+
 
 print(res.status)
 print(res.data)
